13700e749baa761650d343a37cd50a960598fbb3
monkeywise <dev.minhtuan07@gmail.com>
2025-02-11 22:58:53 +0700

    update info

--- Portofolio-v2/src/views/PortfolioView.vue
+++ Portofolio-v2/src/views/PortfolioView.vue
@@ -10,45 +10,46 @@ export default {
           imageUrl: 'portfolio_v2',
           status: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
           tech: 'VueJS 3, Tailwind',
-          github: 'https://github.com/rakha-elctrnx/Portofolio-v2',
-          demo: 'https://bagasrakha.netlify.app/'
+          github: 'https://github.com/TuanMonkey007/My-Profile',
+          demo: 'https://nguyenminhtuan.id.vn'
         },
         {
           id: 2,
-          name: 'Sahabat Muslim',
-          imageUrl: 'sahabatmuslim',
-          status: 'A website that has a responsive display to make it easier for Muslims to read the Quran from anywhere.',
+          name: 'Personal Website',
+          imageUrl: 'portfolio_v2',
+          status: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
           tech: 'VueJS 3, Tailwind',
-          github: 'https://github.com/rakha-elctrnx/muslim-companion',
-          demo: 'https://sahabat-muslim.netlify.app/'
+          github: 'https://github.com/TuanMonkey007/My-Profile',
+          demo: 'https://nguyenminhtuan.id.vn'
         },
         {
           id: 3,
-          name: 'SISTEKDES',
-          imageUrl: 'sistekdes',
-          status: 'Sistem Informasi dan Teknologi Desa, a web application to assist village government work in administration, population data management, community services, etc.',
-          tech: 'Laravel 9, Bootstrap',
-          github: 'null',
-          demo: 'null'
+          name: 'Personal Website',
+          imageUrl: 'portfolio_v2',
+          status: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
+          tech: 'VueJS 3, Tailwind',
+          github: 'https://github.com/TuanMonkey007/My-Profile',
+          demo: 'https://nguyenminhtuan.id.vn'
         },
         {
           id: 4,
-          name: 'Bumdesma',
-          imageUrl: 'bumdesma',
-          status: 'a web application used by village-owned enterprises at the sub-district level to make it easier for them in terms of accounting that is equipped with a microfinance balance.',
-          tech: 'Laravel 8, Bootstrap',
-          github: 'null',
-          demo: 'null'
-        },
-        {
+          name: 'Personal Website',
+          imageUrl: 'portfolio_v2',
+          status: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
+          tech: 'VueJS 3, Tailwind',
+          github: 'https://github.com/TuanMonkey007/My-Profile',
+          demo: 'https://nguyenminhtuan.id.vn'
+        },  {
           id: 5,
-          name: 'SP2SE',
-          imageUrl: 'sp2se',
-          status: 'Sistem Percepatan Pelayanan Surat Elektronik, A web application to manage letters in the village.',
-          tech: 'Codeigniter 3, Bootstrap',
-          github: 'null',
-          demo: 'null'
+          name: 'Personal Website',
+          imageUrl: 'portfolio_v2',
+          status: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
+          tech: 'VueJS 3, Tailwind',
+          github: 'https://github.com/TuanMonkey007/My-Profile',
+          demo: 'https://nguyenminhtuan.id.vn'
         }
+
+       
       ]
     };
   }
