d94e4e0f51edf4a8fbaf04b84e58813107f81772
Nguyễn Minh Tuấn <166813953+TuanMonkey007@users.noreply.github.com>
2025-03-16 10:23:27 +0700

    Update models.py

--- Back-END/appWeb/shortener/models.py
+++ Back-END/appWeb/shortener/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
 from hashids import Hashids
 
-# Khởi tạo Hashids để tạo mã rút gọn
+# Khởi tạo Hashids
 hashids_instance = Hashids(min_length=6, salt="your_secret_salt")
 
 class ShortURL(models.Model):
@@ -12,19 +10,18 @@ class ShortURL(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     access_count = models.IntegerField(default=0)
 
+    def save(self, *args, **kwargs):
+        # Lưu lần đầu để lấy self.id
+        super().save(*args, **kwargs)
+
+        # Tạo short_code nếu chưa có
+        if not self.short_code:
+            while True:
+                new_code = hashids_instance.encode(self.id)
+                if not ShortURL.objects.filter(short_code=new_code).exists():
+                    self.short_code = new_code
+                    super().save(*args, **kwargs)
+                    break
+
     def __str__(self):
         return f"{self.short_code} -> {self.original_url}"
-
-
-@receiver(pre_save, sender=ShortURL)
-def generate_short_code(sender, instance, **kwargs):
-    if not instance.short_code:
-        # Đảm bảo instance.id đã được gán
-        if not instance.id:
-            instance.id = ShortURL.objects.count() + 1
-        
-        while True:
-            new_code = hashids_instance.encode(instance.id)
-            if not ShortURL.objects.filter(short_code=new_code).exists():
-                instance.short_code = new_code
-                break
