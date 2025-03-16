7c38242e9664996bc8d6fd68a876cd009e0faab5
Nguyễn Minh Tuấn <166813953+TuanMonkey007@users.noreply.github.com>
2025-03-16 10:34:33 +0700

    Update models.py add uuid

--- Back-END/appWeb/shortener/models.py
+++ Back-END/appWeb/shortener/models.py
@@ -1,24 +1,24 @@
+import uuid
 from django.db import models
 from hashids import Hashids
 
-# Khởi tạo Hashids
 hashids_instance = Hashids(min_length=6, salt="your_secret_salt")
 
 class ShortURL(models.Model):
-    original_url = models.URLField()
+    original_url = models.URLField(unique=True)
     short_code = models.CharField(max_length=10, unique=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     access_count = models.IntegerField(default=0)
 
     def save(self, *args, **kwargs):
-        if not self.short_code:
-            new_code = hashids_instance.encode(ShortURL.objects.count() + 1)
-
-        # Kiểm tra trùng lặp short_code trong database
-            while ShortURL.objects.filter(short_code=new_code).exists():
-                new_code = hashids_instance.encode(ShortURL.objects.count() + 1)
-
-            self.short_code = new_code
+        # Nếu URL đã tồn tại => Lấy short_code cũ
+        existing_record = ShortURL.objects.filter(original_url=self.original_url).first()
+        if existing_record:
+            self.short_code = existing_record.short_code
+        else:
+            # Nếu URL chưa có => Tạo short_code mới
+            unique_id = str(uuid.uuid4())
+            self.short_code = hashids_instance.encode(int(unique_id[:8], 16))
 
         super().save(*args, **kwargs)
 
